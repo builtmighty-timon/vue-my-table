4c32843671bf84fdd65cb2c4e69b3d1a77875af2
Timon Davis <timon@builtmighty.com>
2025-02-03 10:56:07 +0000

    (ENI-56) WIP - Adds a couple of url params to auto-filter.  This is primarily so that links can be used to take users to the Page with their results.  Who doesn't love a good parlor trick?

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,21 +9,36 @@ document.addEventListener('DOMContentLoaded', () => {
                 {key: 'order_num', label: 'Order #'},
                 {key: 'taker_name', label: 'Test Taker Name'},
                 {key: 'taker_email', label: 'Test Taker Email'},
-                {key: 'expiration_date', label: 'Expiration Date'},
-                {key: 'used_on_date', label: 'Used On Date'},
+                {key: 'expiration_date', label: 'Expires On'},
+                {key: 'used_on_date', label: 'Used On'},
                 {key: 'results', label: 'Results'},
             ],
             data: simpleTableData.tableData, // Comes from php localization
             filters: {
                 testCode: '',
                 expirationStart: '',
-                expirationEnd: '',
+                expirationWithinDays: '',
                 test: '',
                 language: '',
                 search: '',
             },
             sortKey: '',
             sortOrder: 'asc',
+        },
+        created: function() {
+            // Step 1: Parse the "test" parameter from the URL
+            const urlParams = new URLSearchParams(window.location.search);
+            const testFilter = urlParams.get('test');
+            const expiryWithinFilter = urlParams.get('expiration_within_days');
+
+            // Step 2: Set the value in the `filters.test` model
+            if (testFilter) {
+                this.filters.test = testFilter;
+            }
+
+            if (expiryWithinFilter) {
+                this.filters.expirationWithinDays = expiryWithinFilter;
+            }
         },
         methods: {
             uniqueValues(key) {
@@ -40,9 +55,20 @@ document.addEventListener('DOMContentLoaded', () => {
                         if (this.filters.test && row.test !== this.filters.test) return false;
                         if (this.filters.language && row.language !== this.filters.language) return false;
 
+
+
                         const expiration = new Date(row.expiration_date);
+                        const today = new Date();
+                        if (this.filters.expirationWithinDays && expiration) {
+                            if ( expiration > new Date(new Date().setDate(new Date().getDate() + parseInt(this.filters.expirationWithinDays)))) {
+                                return false;
+                            }
+                            if ( expiration < today) {
+                                return false;
+                            }
+                        }
+
                         if (this.filters.expirationStart && expiration < new Date(this.filters.expirationStart)) return false;
-                        if (this.filters.expirationEnd && expiration > new Date(this.filters.expirationEnd)) return false;
 
                         if (this.filters.search) {
                             const searchVal = Object.values(row).join(' ').toLowerCase();
